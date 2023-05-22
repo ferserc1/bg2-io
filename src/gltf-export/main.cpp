c5c0438ccbf0fc426740a0f2402f758d8d93b3de
Fernando Serrano Carpena <ferserc1@gmail.com>
2023-05-22 18:42:17 +0200

    gltf-export dependencies added

--- src/gltf-export/main.cpp
+++ src/gltf-export/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <tiny_gltf.h>
+
 int main(int argc, char** argv)
 {
     std::cout << "Hello, World!" << std::endl;
